3b4a96157939f38b3f49ea4330fb5f19fa5c4e2e
benoansah2025 <memphisperla@gmail.com>
2026-02-08 08:37:26 +0000

    changed the app name in wsgi to fix deployment failed in render

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from app import create_app
 from dotenv import load_dotenv
 load_dotenv()
 
-app = create_app()
+application = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
